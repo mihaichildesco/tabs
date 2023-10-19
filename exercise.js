f5237d10746c4f2e3d3fb0ad1320187cb3a3f4bb
Mihai Childesco <mihai.childesco@gmail.com>
2023-10-19 15:40:40 +0300

    copy js

--- exercise.js
+++ exercise.js
@@ -1,3 +1,21 @@
 const btns = document.querySelectorAll('.tab-btn');
 const about = document.querySelector('.about');
 const articles = document.querySelectorAll('.content');
+
+about.addEventListener('click', (e) => {
+  const idBtn = e.target.dataset.id;
+
+  if (idBtn) {
+    btns.forEach((btn) => {
+      btn.classList.remove('active');
+      e.target.classList.add('active');
+    });
+  }
+
+  articles.forEach((article) => {
+    article.classList.remove('active');
+  });
+
+  const element = document.getElementById(idBtn);
+  element.classList.add('active');
+});
